d4541b069faedeee9b68c765b8c87efaf8568bcc
addnsk <68299837+addnsk@users.noreply.github.com>
2020-07-15 02:34:26 +0700

    init

---
+++ Assets/Scripts/House.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+    [SerializeField] private Thief _thief;
+    [SerializeField] private float _stepVolume;
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_thief.isHouse)
+            _audioSource.volume += _stepVolume;
+        else
+            _audioSource.volume -= _stepVolume;
+    }
+}
---
+++ Assets/Scripts/Thief.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thief : MonoBehaviour
+{
+
+    private Rigidbody2D _rigidbody2D;
+    public bool isHouse;
+
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            _rigidbody2D.AddForce(Vector2.right * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+            _rigidbody2D.AddForce(Vector2.left * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isHouse = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isHouse = false;
+    }
+}
